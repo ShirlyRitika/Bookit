354c2c12f349aec5415695920d44b329838e2ec1
ShirlyRitika <shirlyritikac@gmail.com>
2025-11-08 12:44:08 +0530

    Render port fix

--- backend/server.js
+++ backend/server.js
@@ -33,7 +33,7 @@ app.post("/api/bookings", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>
   console.log(`🚀 Server running at: http://localhost:${PORT}`)
 );
